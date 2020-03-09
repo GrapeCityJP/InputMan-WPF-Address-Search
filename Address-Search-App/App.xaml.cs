f2b0bd5503555197c427a46b70618805008c4d8b
GrapeCity JP Developer Tools Marketing <48461005+GrapeCityJPDevTools@users.noreply.github.com>
2020-03-09 13:34:19 +0900

    added project

---
+++ Address-Search-App/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Address_Search_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Address-Search-App/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GrapeCity.Windows.JPAddress;
+
+namespace Address_Search_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<AddressInfo> addressInfo1 = gcjpaddress1.GetAddress(gcmask1.Value, false, ZipCodeQueryMode.GeneralZipCode);
+            foreach (AddressInfo val in addressInfo1)
+            {
+                gctextbox1.Text = val.Prefecture + val.City + val.Town + val.Area;
+            }
+        }
+    }
+}
